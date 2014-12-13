e742c42576b2079023967354ff6c414e53cec30f
kimberly1295 <kimberly1295@yahoo.com>
2014-12-13 16:08:15 +0800

    gitignored

---
+++ src/java/Servlet/addBook.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Servlet;
+
+import Bean.BookBean;
+import Bean.ProductBean;
+import Bean.ProductLogBean;
+import Bean.UserBean;
+import DAO.Implementation.ProductImplementation;
+import DAO.Implementation.ProductlogImplementation;
+import DAO.Interface.ProductInterface;
+import DAO.Interface.ProductlogInterface;
+import Security.AccessController;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author joechua
+ */
+public class addBook extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession(false); //to ensure that no new session will be created
+        String referer = request.getHeader("Referer");
+        if (session != null) {
+            UserBean user = (UserBean) session.getAttribute("client_user");
+            //String clientToken = (String) session.getAttribute("client_token"); //unused
+            AccessController acl = (AccessController) session.getAttribute("acl"); //unused
+            //UserDAOInterface userIM = new UserDAOImplementation();
+            // String type = userIM.getUserType(user);
+            if (acl.getADD_BOOK() == 1) {
+                ProductInterface pi = new ProductImplementation();
+                ProductlogInterface pli = new ProductlogImplementation();
+                ProductBean pb = new ProductBean();
+                BookBean bb = new BookBean();
+                ProductLogBean plb = new ProductLogBean();
+                bb.setTitle(request.getParameter("addBooktitle"));
+                bb.setSummary(request.getParameter("addBookSummary"));
+                bb.setPrice(Float.parseFloat(request.getParameter("addBookPrice")));
+                bb.setStock(Float.parseFloat(request.getParameter("addBookStock")));
+                bb.setPstatus(Integer.parseInt(request.getParameter("addBookPstatus")));
+                bb.setAuthor(request.getParameter("addBookAuthor"));
+                pb.setPrice(bb.getPrice());
+                pb.setPstatus(bb.getPstatus());
+                pb.setStock(bb.getStock());
+                pb.setSummary(bb.getSummary());
+                pb.setTitle(bb.getTitle());
+                plb.setActivity("Added New Book");
+                plb.setUserID(user.getUserID());
+                pi.addProduct(pb);
+                bb.setProductID(pi.getProductBytitle(pb.getTitle()).getProductID());
+                pb.setProductID(bb.getProductID());
+                pi.addBook(bb);
+                pli.addProductLog(user, pb, plb);
+                try (PrintWriter out = response.getWriter()) {
+                    out.println("added successfully");
+                    //response.sendRedirect("BookManagement.jsp");
+                }
+                
+            } else {
+                response.sendRedirect("Unauthorized.jsp");
+            }
+            //end of authorized below is to catch user without a session 
+        } else {
+            response.sendRedirect("Unauthorized.jsp");
+        }
+
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+
+            out.println("<h1>Servlet addBook at " + request.getContextPath() + "</h1>");
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ web/DVDManagement.jsp
@@ -0,0 +1,58 @@
+<%-- 
+    Document   : DVDManagement
+    Created on : Dec 12, 2014, 9:51:30 PM
+    Author     : joechua
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+  <h1>Hello to DVD management!</h1>
+        <hr>
+         <form class="form-horizontal" name="addJsp" id = "addDVDJsp" method="post" action = "addDVD">
+            <div class="DVD management-choices">
+                Title:<input type="text" name="addDVDtitle" id="addDVDtitle"  placeholder="title"/>
+                <br>Summary:<br>
+               <textarea cols="5" rows="3" name="addDVDSummary" placeholder="summary here..."></textarea>
+                <br>Price:<input type="text" name="addDVDPrice" id="addDVDPrice"  placeholder="P0.0"/>
+                <br>Stock:<input type="text" name="addDVDStock" id="addDVDStock"  placeholder="0.0"/>
+                <br>Status:<input type="text" name="addDVDPstatus" id="addDVDPstatus"  placeholder="1"/>
+                <br>Director:<input type="text" name="addDVDDirector" id="addDVDDirector"  placeholder="pen smith"/>
+                <br><input type="submit" name="addDVDBtn" id="addDVDBtn" value="addDVD"/>
+            </div>
+        </form>
+        <hr>
+        <form class="form-horizontal" name="editProductJsp" id = "editDVDJsp" method="post" action = "editDVD">
+            <div class="DVD management-choices">
+                Title:<input type="text" name="editDVDtitle" id="editDVDtitle"  placeholder="title"/>
+                <br>Summary:<br>
+               <textarea cols="5" rows="3" name="editDVDSummary" placeholder="summary here..."></textarea>
+                <br>Price:<input type="text" name="editDVDPrice" id="editDVDPrice"  placeholder="P0.0"/>
+                <br>Stock:<input type="text" name="editDVDStock" id="editDVDStock"  placeholder="0.0"/>
+                <br>Status:<input type="text" name="editDVDPstatus" id="editDVDPstatus"  placeholder="1"/>
+                <br>Director:<input type="text" name="editDVDDirector" id="editDVDDirector"  placeholder="pen smith"/>
+                <br><input type="submit" name="editDVDBtn" id="editDVDBtn" value="editDVD"/>
+                <input type="submit" name="editDVDBtn" id="editDVDBtn" value="editDVD"/>
+            </div>
+        </form>
+        <hr>
+        <form class="form-horizontal" name="deleteProductJsp" id = "deleteDVDJsp" method="post" action = "deleteDVD">
+            <div class="DVD management-choices">
+                Title:<input type="text" name="delDVDtitle" id="delDVDtitle"  placeholder="title"/>
+                <br>Summary:<br>
+               <textarea cols="5" rows="3" name="delDVDSummary" placeholder="summary here..."></textarea>
+                <br>Price:<input type="text" name="delDVDPrice" id="delDVDPrice"  placeholder="P0.0"/>
+                <br>Stock:<input type="text" name="delDVDStock" id="delDVDStock"  placeholder="0.0"/>
+                <br>Status:<input type="text" name="delDVDPstatus" id="delDVDPstatus"  placeholder="1"/>
+                <br>Director:<input type="text" name="delDVDDirector" id="delDVDDirector"  placeholder="pen smith"/>
+                <br><input type="submit" name="delDVDBtn" id="delDVDBtn" value="delDVD"/>
+                <input type="submit" name="deleteDVDBtn" id="deleteDVDBtn" value="deleteDVD"/>
+            </div>
+        </form>
+    </body>
+</html>
